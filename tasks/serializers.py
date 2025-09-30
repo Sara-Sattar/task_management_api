d4b2579bc1be91de9fef1a79f25dde45805cefc0
Sara <sarasarra36@icloud.com>
2025-09-30 21:29:32 +0100

    Add Tasks API and include under /api/

---
+++ tasks/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'due_date', 'priority', 'status', 'created_at', 'user']
+        read_only_fields = ['id', 'created_at', 'user']
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and request.user and request.user.is_authenticated:
+            validated_data['user'] = request.user
+        return super().create(validated_data)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+
 
 # Create your views here.
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Task.objects.all()
+        return Task.objects.filter(user=self.request.user)
