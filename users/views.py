12220f0c021b49f2bfbacce5a2ed6efb57e301bb
Sara <sarasarra36@icloud.com>
2025-10-16 23:00:28 +0100

    Refactored UserViewSet to use generic views (ListCreate and RetrieveUpdateDestroy)

--- users/views.py
+++ users/views.py
@@ -1,18 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 
 User = get_user_model()
 
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+class UserListCreateView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
-        # Users can only see their own profile unless they're staff
-        if self.request.user.is_staff:
-            return User.objects.all()
+        return User.objects.filter(id=self.request.user.id)
+
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
         return User.objects.filter(id=self.request.user.id)
