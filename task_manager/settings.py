7e529efe04df92726a6ffea1ae17ee350b4e07fb
Sara <sarasarra36@icloud.com>
2025-10-18 16:02:48 +0100

    Configure settings and requirements.txt for deployment

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-twh9*^56b)$f9n9us03irb-07^y457hk*v_lq9ms5g0269z#2c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SattarSara.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
